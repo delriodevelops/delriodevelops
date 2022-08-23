bb44259a8decb07e102032cd48145993e1e1dc04
delrio <delriodeveloper@gmail.com>
2022-08-23 19:55:26 +0200

    metas and stuff

--- blocks/About.jsx
+++ blocks/About.jsx
@@ -70,7 +70,7 @@ const About = () => {
     </div>
     <Link href="/projects">
       <a >
-        <button className="flex mx-auto mt-20 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:bg-blue-500 rounded text-lg items-center gap-2">See my work <BsCodeSlash/> </button>
+        <button className="flex mx-auto mt-20 text-white bg-blue-600 border-0 py-2 px-8 focus:outline-none hover:bg-blue-500 rounded text-lg items-center gap-2">See my work</button>
       </a>
     </Link>
   </div>
--- blocks/Layout.jsx
+++ blocks/Layout.jsx
@@ -11,6 +11,11 @@ const Layout = ({children}) => {
             <title>Del Rio</title>
             <link rel="icon" type="image/x-icon" href="favicon.ico"/>
             <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
+            <meta property="og:title" content="Christian del Rio Rodriguez's Portfolio 2022"/>
+            <meta property="og:type" content="Portfolio"/>
+            <meta property="og:url" content="https://www.iamdelrio.com"/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta property="og:type" content="website"/>
         </Head>
         <Header/>
             {children}
