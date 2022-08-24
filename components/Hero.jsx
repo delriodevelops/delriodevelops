63f86cc1a13253953f69cc955a3297ca9708f45e
Christian del Rio Rodriguez <delriodeveloper@gmail.com>
2022-08-24 04:22:40 +0200

    xd

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
     <div className="max-w-3xl mx-auto text-center">
       <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-          Full Stack Developer
+          Full Stack Developer<br/>
         <span className="sm:block">
         Christian del Rio Rodriguez
         </span>
